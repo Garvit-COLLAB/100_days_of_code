5cada14c0ae3c582206069a86032b7816055f23a
Garvit Jain <garvitjain2007gj@gmail.com>
2025-08-30 17:50:21 +0530

    a

--- even_odd.c
+++ even_odd.c
@@ -11,7 +11,6 @@ int main (){
     } else {
         printf("%d is an odd integer.",a);
     }
+
     return 0;
 }
-
-
--- sign.c
+++ sign.c
@@ -15,5 +15,4 @@ int main(){
     }else {
         printf("%d is negative integer.",a);
     }
- return 0;
 }
