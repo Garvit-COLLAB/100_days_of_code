5cbabc42c0c73afb37d3debdfe75542003597999
Garvit Jain <garvitjain2007gj@gmail.com>
2025-09-01 20:22:06 +0530

    program to assign grades based on a percentage input.

--- grade.c
+++ grade.c
@@ -6,18 +6,19 @@ int main() {
     printf("Enter the percentage obtained by you:");
     scanf("%d",&a);
      
-    if (a >= 90){
+if (a >= 90 && a <= 100){
         printf("Your grade is A.");
-    }else if (a >= 80){
+    }else if (a >= 80 && a < 90){
         printf("Your grade is B.");
-    }else if (a >= 70){
+    }else if (a >= 70 && a < 80){
         printf("Your grade is C.");
-    }else if (a > 50){
+    }else if (a >=50 && a < 70){
         printf("Your grade is D.");
-    }else {
+    }else if (a >= 0 && a < 50){
         printf("Your grade is F.");
     }
     return 0;  
 }
+
 
 
